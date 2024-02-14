0b039e0f5fb4242cd017df03d475a3202e7be2fa
Anne <adhiamboanne699@gmail.com>
2024-02-14 14:32:52 +0300

    Constructors and Inheritance in java

---
+++ src/Cons2.java
@@ -0,0 +1,6 @@
+public class Cons2 {
+    String sentance ="I automatically call my self";
+    public void  read(){
+        System.out.println (sentance);
+    }
+}
---
+++ src/Constractor.java
@@ -0,0 +1,11 @@
+
+public class Constractor extends Cons2{
+    Constractor () {
+        System.out.println("I am a constractor");
+
+    }
+    public static void main(String[]args){
+Constractor cons= new Constractor();
+cons.read();
+    }
+}
